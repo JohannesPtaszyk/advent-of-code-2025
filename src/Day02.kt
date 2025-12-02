03ca85ce0371e1442fa2bfe06fae503e44433e7e
Johannes Ptaszyk <johannes@ptaszyk.de>
2025-12-02 13:30:37 +0100

    add day 2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -20,8 +20,36 @@ fun main() {
             .sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        fun String.isInvalid(): Boolean {
+            val first = substring(0, length / 2)
+            val patterns = mutableListOf<String>()
+            for (i in 1..first.length) {
+                patterns += first.take(i)
+            }
+            val patternMatches =  patterns
+                .map { pattern ->
+                    if(length % pattern.length == 0) {
+                        windowed(pattern.length, pattern.length).all { it == pattern }
+                    } else {
+                        false
+                    }
+                }
+            return patternMatches.any { it }
+        }
+
+        return input
+            .first()
+            .split(",")
+            .map { pair -> pair.split("-") }
+            .also { it.println() }
+            .filter { strings -> strings.none { it.toLongOrNull() == null } }
+            .flatMap { ids ->
+                val idRange = ids.first().toLong()..ids.last().toLong()
+                idRange.filter { it.toString().isInvalid() }.also { it.println() }
+            }
+            .also { it.println() }
+            .sum()
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
@@ -37,7 +65,7 @@ fun main() {
 
     val part2 = part2(testInput)
     println("Test Part 2: $part2")
-    check(part2 == 6)
+    check(part2 == 4174379265L)
 
     ("Part 2: " + part2(input)).println()
 }
